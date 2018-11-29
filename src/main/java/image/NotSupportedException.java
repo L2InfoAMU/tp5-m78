93cf30894109c413ec154e4eabecaa5162d9a95d
renaud <renaud.toungstoune@gmail.com>
2018-11-29 17:36:31 +0100

    ajout raster flag factory

--- src/main/java/image/NotSupportedException.java
+++ src/main/java/image/NotSupportedException.java
@@ -3,5 +3,6 @@ package image;
 public class NotSupportedException extends RuntimeException{
     public NotSupportedException (String exception) {
         super(exception);
+        //TODO verifier si c'est pas n'importe quoi
     }
 }
