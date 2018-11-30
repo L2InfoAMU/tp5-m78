0288416b8f670bad300ae4decf0873548ea0c607
renaud <renaud.toungstoune@gmail.com>
2018-11-30 18:00:52 +0100

    accompli les tests pour sparse image, le code fonctionne

--- src/main/java/viewer/Display.java
+++ src/main/java/viewer/Display.java
@@ -26,7 +26,9 @@ public class Display implements Initializable {
         //imageFactory = new RasterUniformImageFactory(200, 200, Color.RED, RasterImageType.BRUTE);
         //imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.BRUTE);
         //imageFactory = new RasterUniformImageFactory(200, 200, Color.RED, RasterImageType.PALETTE);
-        imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.PALETTE);
+        //imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.PALETTE);
+        //imageFactory = new RasterUniformImageFactory(200, 200, Color.RED, RasterImageType.SPARSE);
+        imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.SPARSE);
         this.image = imageFactory.makeImage();
 
         render();
